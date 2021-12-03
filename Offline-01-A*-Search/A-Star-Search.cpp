83a4c816a1782ce8fa8d237b3b85230a376b208b
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-12-03 17:08:42 +0600

    hamming and manhattan worked

--- Offline-01-A*-Search/A-Star-Search.cpp
+++ Offline-01-A*-Search/A-Star-Search.cpp
@@ -8,40 +8,90 @@ using namespace std;
 class Board{
     int k;
     vector<vector<int>> square;
-    vector<Board> adjBoard;
     bool visited;
+    int gn;
+    int hn;
+    Board* parent;
     public:
     Board(int k){
         this->k=k;
         square.resize(k);
         visited=false;
+        parent = NULL;
     }
+
+
+    void setk(int k){
+        this->k = k;
+    }
+
     int getk(){
         return k;
+    }
+
+    void setParent(Board* b){
+        parent = b;
+    }
+
+    Board* getParent(){
+        return parent;
+    }
+
+    void setGn(int gn){
+        this->gn = gn;
+    }
+
+    void setHn(int hn){
+        this->hn = hn;
+    }
+
+    int getGn(){
+        return gn;
+    }
+
+    int getHn(){
+        return hn;
     }
 
     void insertValue(int row, int val){
         square[row].push_back(val);
     }
 
+    vector<vector<int>> getGrid(){
+        return square;
+    }
+
     int getCorrectRow(int val){
-        return val/k;
+        return (val-1)/k;
     }
 
     int getCorrectColumn(int val){
-        return val%k;
+        return (val-1)%k;
     }
 
-    int hammingDistance(){
-        int distance=0;
-        for(int i=0; i<square.size(); i++){
-            for(int j=0; j<square[i].size(); j++){
+    int manhattan(){
+        int manhattanDist=0;
+        for(int i=0; i<k; i++){
+            for(int j=0; j<k; j++){
                 if(square[i][j] != 0){
-                    distance+= abs(getCorrectRow(square[i][j])-i)+abs(getCorrectColumn(square[i][j])-j);
+                    manhattanDist+= abs(getCorrectRow(square[i][j])-i)+abs(getCorrectColumn(square[i][j])-j);
                 }
             }
         }
-        return distance;
+        return manhattanDist;
+    }
+
+    int hammingDistance(){
+        int hammingDistance=0;
+        for(int i=0; i<k; i++){
+            for(int j=0; j<k; j++){
+                if(square[i][j] != 0){
+                    int should_be = i * k + j + 1;
+                    hammingDistance += (should_be != square[i][j]);
+                }
+            }
+        }
+        return hammingDistance;
     }
     void printBoard(){
         cout<<"----"<<endl;
@@ -52,12 +102,6 @@ class Board{
             cout<<endl;
         }
         cout<<"----"<<endl;
-    }
-
-    void printAdjBoard(){
-        for(auto i:adjBoard){
-            i.printBoard();
-        }
     }
 
     void swapVal(int rowSrc, int rowDest, int colSrc, int colDest){
@@ -72,53 +116,133 @@ class Board{
         visited = true;
     }
 
-    void getAdjBoard(){
-        
-        for(int i=0; i<k; i++){
-            for(int j=0; j<k; j++){
-                if(square[i][j] == 0){
-                    if(i>0 and i<k-1){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i+1, j, j);
-                        adjBoard.push_back(temp);
-                        temp = *this;
-                        temp.swapVal(i, i-1, j, j);
-                        adjBoard.push_back(temp);
+    bool operator==(Board *b){
+        bool yes=true;
+        vector<vector<int>> sq = b->getGrid();
+        for(int i=0; i<square.size(); i++){
+            for(int j=0; j<square[i].size(); j++){
+                if(sq[i][j] != square[i][j]){
+                    yes=false;
+                    break;
+                }
+            }
+        }
+        return yes;
+    }
+};
+
+struct Compare
+{
+    bool operator()(Board *a, Board *b){
+        int cost1 = a->getGn()+a->getHn();
+        int cost2 = b->getGn()+b->getHn();
+        return cost1>cost2;
+    }
+};
+
+
+class Graph {
+    Board *board;
+    int k;
+    public:
+
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    bool isOutSide(int x, int y){
+        if(x < 0 or x >= k or y<0 or y>=k ) return true;
+        return false;
+    }
+
+    Graph(int k){
+        board = new Board(k);
+        this->k = k;
+    }
+
+    void insertValue(int row, int val){
+        board->insertValue(row, val);
+    }
+
+    void printBoard(){
+        board->printBoard();
+    }
+
+    void A_Star_Search(){
+        board->setGn(0);
+        priority_queue<Board*, vector<Board*>, Compare>pq;
+        pq.push(board);
+        cout<<"pushed"<<endl;
+
+        unordered_set<Board*> visited;
+
+        while(!pq.empty()){
+            Board *topBoard = pq.top();
+            topBoard->printBoard();
+            pq.pop();
+            visited.insert(topBoard);
+            cout<<"DHUKSE"<<endl;
+
+            vector<vector<int>> v {
+                {
+                    1, 2, 3
+                },
+                {
+                    4, 5, 6
+                },{
+                7, 8, 0}
+
+            };
+
+            vector<vector<int>> goal;
+            int cnt=1;
+            goal.resize(k);
+            for(int i=0; i<k; i++){
+                for(int j=0; j<k; j++){
+                    goal[i].push_back(cnt);
+                    cnt++;
+                }
+            }
+            goal[k-1][k-1] = 0;
+
+            if(topBoard->getGrid() == goal){
+            //if(topBoard->hammingDistance()==0){
+                cout<<"Done"<<endl;
+                cout<<"Hamming Distance: "<<topBoard->hammingDistance()<<endl;
+                cout<<"Manhattan Distance: "<<topBoard->manhattan()<<endl;
+                topBoard->printBoard();
+                break;
+            }
+
+            int zeroX = -1;
+            int zeroY = -1;
+            for(int i=0; i<k; i++){
+                for(int j=0; j<k; j++){
+                    if(topBoard->getGrid()[i][j]==0){
+                        zeroX = i;
+                        zeroY = j;
+                        break;
                     }
-                    else if(i==0){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i+1, j, j);
-                        adjBoard.push_back(temp);
+                }
+            }
+
+            for(int i=0; i<4; i++){
+
+                int new_x = zeroX+dx[i], new_y = zeroY+dy[i];
+                if(isOutSide(new_x, new_y)) continue;
+
+                Board *newBoard = new Board(k);
+                for(int i=0; i<k; i++){
+                    for(int j=0; j<k; j++){
+                        newBoard->insertValue(i, topBoard->getGrid()[i][j]);
                     }
-                    else if(i==k-1){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i-1, j, j);
-                        adjBoard.push_back(temp);
-                    }
-                    if(j>0 and j<k-1){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i, j, j+1);
-                        adjBoard.push_back(temp);
-                        temp = *this;
-                        temp.swapVal(i, i, j, j-1);
-                        adjBoard.push_back(temp);
-                    }
-                    else if(j==0){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i, j, j+1);
-                        adjBoard.push_back(temp);
-                    }
-                    else if(j==k-1){
-                        Board temp(k);
-                        temp = *this;
-                        temp.swapVal(i, i, j, j-1);
-                        adjBoard.push_back(temp);
-                    }
+                }
+                newBoard->swapVal(zeroX, new_x, zeroY, new_y );
+                newBoard->setParent(topBoard);
+                newBoard->setGn(newBoard->getParent()->getGn()+1);
+                newBoard->setHn(newBoard->hammingDistance());
+
+                if(visited.find(newBoard)==visited.end()){
+                    pq.push(newBoard);
                 }
             }
         }
@@ -128,10 +252,10 @@ class Board{
 
 int main(){
     FileIO;
-    
+
     int k;
     cin>>k;
-    Board *board = new Board(k);
+    Graph *graph = new Graph(k);
     for(int i=0; i<k; i++){
         for(int j=0; j<k; j++){
             string str;
@@ -139,19 +263,14 @@ int main(){
             cin>>str;
             if(str != "*"){
                 val = stoi(str);
-                board->insertValue(i, val);
+                graph->insertValue(i, val);
             }else{
-                board->insertValue(i, 0);
+                graph->insertValue(i, 0);
             }
         }
     }
-    cout<<"----"<<endl;
-    board->printBoard();
-    cout<<"----"<<endl;
-    board->getAdjBoard();
-    cout<<"----"<<endl;
-    board->printAdjBoard();
-    cout<<"----"<<endl;
-    board->printBoard();
+    cout<<"Inserted from main"<<endl;
+    graph->printBoard();
+    graph->A_Star_Search();
     return 0;
 }
