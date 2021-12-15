69c81b174145ef53352d2fddf5d5bac15f17d3bc
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-12-16 03:34:32 +0600

    code modified

--- Offline-02-Adversarial-Search/adversarial_search.cpp
+++ Offline-02-Adversarial-Search/adversarial_search.cpp
@@ -9,11 +9,15 @@ using namespace std;
 #define INITIAL_STONES 4
 #define MANCALA_0 6
 #define MANCALA_1 13
+#define W1 10
+#define W2 15
+#define W3 20
 
 class Mancala{
     public:
     vector<int>bins;
     int turn;
+    bool gameOver;
     Mancala(){
         bins.resize(NUM_OF_BINS);
         for(int i=0; i<NUM_OF_BINS; i++){
@@ -22,6 +26,7 @@ class Mancala{
         bins[MANCALA_0] = 0;
         bins[MANCALA_1] = 0;
         turn = 0;
+        gameOver = false;
     }
     void printGameState(){
         cout<<"___\t___\t___\t___\t___\t___\t___\t___\t___\t___\t\n"<<endl;
@@ -52,24 +57,11 @@ class Mancala{
                 cout<<"no stones in bin "<<val<<" ,discarding input"<<endl;
                 return;
             }
-            bins[input] = 0;
-            for(int i=1; i<=val; i++){
-                if((input+i)%NUM_OF_BINS != 13){
-                    bins[(input+i)%NUM_OF_BINS]++;
-                }
-                else{
-                    val++;
-                }
-            }
-
-            if(bins[(input+val)%NUM_OF_BINS]==1 and (input+val)%NUM_OF_BINS != 6 and bins[12-(input+val)%NUM_OF_BINS]!=0){
-                bins[6] += bins[(input+val)%NUM_OF_BINS] + bins[12-(input+val)%NUM_OF_BINS] ;
-                bins[12-(input+val)%NUM_OF_BINS]=0;
-                bins[(input+val)%NUM_OF_BINS]=0;
-            }
-
+            distributeStones(MANCALA_1, input, val);
+            grabStones(MANCALA_0, input, val);
             printGameState();
-            if((input+val)%NUM_OF_BINS!=6){
+
+            if((input+val)%NUM_OF_BINS!=MANCALA_0){
                 turn = 1-turn;
             }
             else{
@@ -84,24 +76,11 @@ class Mancala{
                 cout<<"no stones in bin "<<val<<" ,discarding input"<<endl;
                 return;
             }
-            bins[input] = 0;
-            for(int i=1; i<=val; i++){
-                if((input+i)%NUM_OF_BINS != 6){
-                    bins[(input+i)%NUM_OF_BINS]++;
-                }
-                else{
-                    val++;
-                }
-            }
-
-            if(bins[(input+val)%NUM_OF_BINS]==1 and (input+val)%NUM_OF_BINS != 13 and bins[12-(input+val)%NUM_OF_BINS]!=0){
-                bins[13] += bins[(input+val)%NUM_OF_BINS] + bins[12-(input+val)%NUM_OF_BINS];
-                bins[12-(input+val)%NUM_OF_BINS]=0;
-                bins[(input+val)%NUM_OF_BINS]=0;
-            }
-
+            distributeStones(MANCALA_0, input, val);
+            grabStones(MANCALA_1, input, val);
             printGameState();
-            if((input+val)%NUM_OF_BINS != 13){
+
+            if((input+val)%NUM_OF_BINS != MANCALA_1){
                 turn = 1-turn;
             }
             else{
@@ -109,10 +88,28 @@ class Mancala{
             }
         }
 
-        bool done = rowEmpty();
-        if(rowEmpty()){
-            printGameState();
-            getWinner();
+        gameOver = rowEmpty();
+    }
+
+    void distributeStones(int avoid, int input, int& val){
+        bins[input] = 0;
+        for(int i=1; i<=val; i++){
+            if((input+i)%NUM_OF_BINS != avoid){
+                bins[(input+i)%NUM_OF_BINS]++;
+                // cout<<(input+i)%NUM_OF_BINS<<" ";
+            }
+            else{
+                val++;
+            }
+        }
+        // cout<<endl;
+    }
+
+    void grabStones(int to, int input, int val){
+        if(bins[(input+val)%NUM_OF_BINS]==1 and (input+val)%NUM_OF_BINS != to and bins[12-(input+val)%NUM_OF_BINS]!=0){
+            bins[to] += bins[(input+val)%NUM_OF_BINS] + bins[12-(input+val)%NUM_OF_BINS] ;
+            bins[12-(input+val)%NUM_OF_BINS]=0;
+            bins[(input+val)%NUM_OF_BINS]=0;
         }
     }
 
@@ -157,7 +154,50 @@ class Mancala{
         cout<<"Player 1 : "<<bins[13]<<endl;
 
     }
+
+    int heuristic1(){
+        if(turn==0){
+            return bins[6]-bins[13];
+        }
+        else{
+            return bins[13]-bins[6];
+        }
+    }
+
+    int heuristic2(){
+        return W1*heuristic1() + W2*heuristic2();
+    }
+
+    int heuristic3(){
+        return heuristic2()+W3;
+    }
+
+    int evalHeuristic(int heuristicNo){
+        if(heuristicNo==1){
+            return heuristic1();
+        }
+        else if(heuristicNo==2){
+            return heuristic2();
+        }
+        else if(heuristicNo==3){
+            return heuristic3();
+        }
+    }
+
+    // int minimaxAlgorithm(int depth, int isMax, int alpha, int beta, int h){
+    //     if(depth==0){
+    //         return heuristic1();
+    //     }
+
+    //     vector<int>copyBins;
+    //     copyBins.resize(NUM_OF_BINS);
+    //     for(int i=0; i<NUM_OF_BINS; i++){
+    //         copyBins[i] = bins[i];
+    //     }
+    // }
+
 };
+
 
 int main(){
     FileIO;
@@ -165,20 +205,14 @@ int main(){
     mancala->printGameState();
     //cout<<"Turn of 0"<<endl;
     int input;
-    while(1){
+    while(mancala->gameOver==false){
         cout<<"Turn : "<<mancala->turn<<endl;
-
-        if(mancala->rowEmpty()==false){
-            cin>>input;
-            cout<<"Bin : "<<input<<endl;
-            if(input==0)
-                break;
-
-            mancala->chooseBin(input);
-        }
-        else{
-            break;
-        }
+        cin>>input;
+        cout<<"Bin : "<<input<<endl;
+        mancala->chooseBin(input);     
     }
+    mancala->getWinner();
+    mancala->printGameState();
+    cout<<"GAME OVER"<<endl;
     return 0;
 }
