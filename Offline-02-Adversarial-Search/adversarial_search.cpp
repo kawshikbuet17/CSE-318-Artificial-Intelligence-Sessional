20fb9613576717dcdb3fb9659e2fc44424ff1ba9
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-12-17 00:21:11 +0600

    bug in grabStones( )

--- Offline-02-Adversarial-Search/adversarial_search.cpp
+++ Offline-02-Adversarial-Search/adversarial_search.cpp
@@ -5,6 +5,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define INF 100000
 #define NUM_OF_BINS 14
 #define INITIAL_STONES 4
 #define MANCALA_0 6
@@ -18,6 +19,8 @@ class Mancala{
     vector<int>bins;
     int turn;
     bool gameOver;
+    int maxEva;
+    int minEva;
     Mancala(){
         bins.resize(NUM_OF_BINS);
         for(int i=0; i<NUM_OF_BINS; i++){
@@ -27,6 +30,7 @@ class Mancala{
         bins[MANCALA_1] = 0;
         turn = 0;
         gameOver = false;
+
     }
     void printGameState(){
         cout<<"___\t___\t___\t___\t___\t___\t___\t___\t___\t___\t\n"<<endl;
@@ -51,44 +55,42 @@ class Mancala{
 
     void chooseBin(int input){
         if(turn==0){
-            input--;
             int val = bins[input];
             if(val==0){
                 cout<<"no stones in bin "<<val<<" ,discarding input"<<endl;
                 return;
             }
             distributeStones(MANCALA_1, input, val);
-            grabStones(MANCALA_0, input, val);
-            printGameState();
+            //grabStones(MANCALA_0, input, val);
+
 
             if((input+val)%NUM_OF_BINS!=MANCALA_0){
                 turn = 1-turn;
             }
             else{
-                cout<<"turn not changed"<<endl;
+                ;//cout<<"turn not changed"<<endl;
             }
         }
 
         else if(turn==1){
-            input+=6;
             int val = bins[input];
             if(val==0){
                 cout<<"no stones in bin "<<val<<" ,discarding input"<<endl;
                 return;
             }
             distributeStones(MANCALA_0, input, val);
-            grabStones(MANCALA_1, input, val);
-            printGameState();
+            //grabStones(MANCALA_1, input, val);
+
 
             if((input+val)%NUM_OF_BINS != MANCALA_1){
                 turn = 1-turn;
             }
             else{
-                cout<<"turn not changed"<<endl;
+                ;//cout<<"turn not changed"<<endl;
             }
         }
 
-        gameOver = rowEmpty();
+
     }
 
     void distributeStones(int avoid, int input, int& val){
@@ -96,13 +98,13 @@ class Mancala{
         for(int i=1; i<=val; i++){
             if((input+i)%NUM_OF_BINS != avoid){
                 bins[(input+i)%NUM_OF_BINS]++;
-                // cout<<(input+i)%NUM_OF_BINS<<" ";
+                //cout<<(input+i)%NUM_OF_BINS<<" ";
             }
             else{
                 val++;
             }
         }
-        // cout<<endl;
+        //cout<<endl;
     }
 
     void grabStones(int to, int input, int val){
@@ -155,7 +157,7 @@ class Mancala{
 
     }
 
-    int heuristic1(){
+    int heuristic1(int turn){
         if(turn==0){
             return bins[6]-bins[13];
         }
@@ -164,55 +166,128 @@ class Mancala{
         }
     }
 
-    int heuristic2(){
-        return W1*heuristic1() + W2*heuristic2();
+    int heuristic2(int turn){
+        return W1*heuristic1(turn) + W2*heuristic1(turn);
     }
 
-    int heuristic3(){
-        return heuristic2()+W3;
+    int heuristic3(int turn){
+        return heuristic2(turn)+W3;
     }
 
-    int evalHeuristic(int heuristicNo){
+    int evalHeuristic(int heuristicNo, int turn){
         if(heuristicNo==1){
-            return heuristic1();
+            return heuristic1(turn);
         }
         else if(heuristicNo==2){
-            return heuristic2();
+            return heuristic2(turn);
         }
         else if(heuristicNo==3){
-            return heuristic3();
+            return heuristic3(turn);
         }
     }
 
-    // int minimaxAlgorithm(int depth, int isMax, int alpha, int beta, int h){
-    //     if(depth==0){
-    //         return heuristic1();
-    //     }
+    vector<int> backupBins(vector<int> v){
+        vector<int>backup;
+        backup.resize(NUM_OF_BINS);
+        for(int i=0; i<NUM_OF_BINS; i++){
+            backup[i] = v[i];
+        }
+        return backup;
+    }
 
-    //     vector<int>copyBins;
-    //     copyBins.resize(NUM_OF_BINS);
-    //     for(int i=0; i<NUM_OF_BINS; i++){
-    //         copyBins[i] = bins[i];
-    //     }
-    // }
+    pair<int,int> minimaxAlgorithm(int depth, int turn, int alpha, int beta){
+        if(depth==0){
+            return make_pair(evalHeuristic(1, turn), 1);
+        }
+        if(gameOver==true){
+            return make_pair(evalHeuristic(1, turn), 1);
+        }
 
+        int eva;
+        int index = -1;
+        if(turn){
+            maxEva = -INF;
+            for(int i=7; i<13; i++){
+                if(bins[i]==0){
+                    continue;
+                }
+                vector<int> b = bins;
+                int turnBackup = this->turn;
+                chooseBin(i);
+                eva = minimaxAlgorithm(depth-1, this->turn, 0, 0).first;
+                maxEva = max(maxEva, eva);
+                if(eva==maxEva){
+                    index=i;
+                }
+                bins = b;
+                this->turn = turnBackup;
+            }
+            return make_pair(maxEva, index);
+        }
+        else{
+            minEva = INF;
+            for(int i=0; i<6; i++){
+                if(bins[i]==0){
+                    continue;
+                }
+                vector<int> b = bins;
+                int turnBackup = this->turn;
+                chooseBin(i);
+                eva = minimaxAlgorithm(depth-1, this->turn, 0, 0).first;
+                minEva = min(minEva, eva);
+                if(eva==minEva){
+                    index=i;
+                }
+                bins = b;
+                this->turn = turnBackup;
+            }
+            return make_pair(minEva,index);
+        }
+    }
 };
 
 
 int main(){
-    FileIO;
+    //FileIO;
     Mancala *mancala = new Mancala();
     mancala->printGameState();
-    //cout<<"Turn of 0"<<endl;
     int input;
     while(mancala->gameOver==false){
         cout<<"Turn : "<<mancala->turn<<endl;
-        cin>>input;
-        cout<<"Bin : "<<input<<endl;
-        mancala->chooseBin(input);     
+
+        if(mancala->turn==0){
+            cin>>input;
+            cout<<"Bin : "<<input<<endl;
+            mancala->chooseBin(input-1);
+            mancala->printGameState();
+            mancala->gameOver = mancala->rowEmpty();
+        }
+        else{
+            ///Play with computer
+
+            int index = mancala->minimaxAlgorithm(7, mancala->turn, 0, 0).second;
+            cout<<"Bin : "<<index<<endl;
+            //cout<<"Printing current BInS"<<endl;
+            //cout<<"TURN___"<<mancala->turn<<endl;
+            //mancala->printGameState();
+            //cout<<"Printing current BInE"<<endl;
+            mancala->chooseBin(index);
+            mancala->printGameState();
+            mancala->gameOver = mancala->rowEmpty();
+
+            //Play with player
+            //cin>>input;
+            //cout<<"Bin : "<<input<<endl;
+            //mancala->chooseBin(input);
+            //mancala->printGameState();
+            //mancala->gameOver = mancala->rowEmpty();
+        }
     }
     mancala->getWinner();
     mancala->printGameState();
     cout<<"GAME OVER"<<endl;
     return 0;
 }
+
+///turn 0, player
+///turn 1, PC
